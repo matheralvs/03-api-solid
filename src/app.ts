a80d5b0b9a6249f149d36ce2a48cb48b11e8c123
Matheus Alves <mtalves27@gmail.com>
2023-11-02 18:42:36 -0300

    chore: use-cases and design patterns

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
-import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
+import fastify from 'fastify'
+import { ZodError } from 'zod'
 
-export const app = fastify();
+import { env } from './env'
+import { appRoutes } from './http/routes'
 
-const prisma = new PrismaClient();
+export const app = fastify()
+
+app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if(error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if(env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
+})
